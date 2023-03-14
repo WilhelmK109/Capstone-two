2aeceb29ce8e8ec8323ffb445ad516df5ddb86b1
isaacg <iwanyoike65@gmail.com>
2023-03-14 17:50:17 +0300

    create function to show popup and to remove the pop up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './style.css';
 
-button.addEventListener('click',()=>{
-    
+const close = document.querySelector('.button-close');
+const open = document.querySelector('.open-comments');
+
+open.addEventListener('click', () => {
+    document.querySelector('.comment-container').style.display = 'block';
+    document.querySelector('.relative').style.display = 'none';
+});
+
+close.addEventListener('click',()=>{
+    document.querySelector('.comment-container').style.display = 'none';
+    document.querySelector('.relative').style.display = 'block';
 });
