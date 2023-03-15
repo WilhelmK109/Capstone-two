b39de85505ffe325206375c8b9b9e1d748bb5ac7
isaacg <iwanyoike65@gmail.com>
2023-03-15 11:04:08 +0300

    Add test to the count comment list function

---
+++ test/test.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import countListItems from "./tester.js";
+
+test("countListItems", () => {
+    document.body.innerHTML = `
+    <ul class="commentlist">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    `;
+    const commentlist = document.querySelector(".commentlist");
+    expect(countListItems(commentlist)).toBe(3);
+}
+);
